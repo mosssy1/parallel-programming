31e1e28b2cf759f9e9caca2374870d605115a81f
mosssy1 <arina021003@gmail.com>
2025-09-19 17:36:41 +0300

    add 6 lab

---
+++ lab6_3/lab6_3/lab6_3.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <omp.h>
+
+const int N = 40;
+
+void initializeMatrix(int matrix[N][N]) 
+{
+    for (int i = 0; i < N; ++i) 
+    {
+        for (int j = 0; j < N; ++j) 
+        {
+            matrix[i][j] = rand() % 100;
+        }
+    }
+}
+
+void matrixMultiply(int A[N][N], int B[N][N], int result[N][N]) 
+{
+#pragma omp parallel for
+    for (int i = 0; i < N; ++i) 
+    {
+        for (int j = 0; j < N; ++j) 
+        {
+            result[i][j] = 0;
+            for (int k = 0; k < N; ++k) 
+            {
+                result[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+int main() 
+{
+    std::srand(std::time(0));
+
+    int A[N][N];
+    int B[N][N];
+    int result[N][N];
+
+    initializeMatrix(A);
+    initializeMatrix(B);
+
+
+    matrixMultiply(A, B, result);
+
+    std::cout << "matrix A:" << std::endl;
+    for (int i = 0; i < N; ++i) 
+    {
+        for (int j = 0; j < N; ++j) 
+        {
+            std::cout << A[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "\nmatrix B:" << std::endl;
+    for (int i = 0; i < N; ++i) 
+    {
+        for (int j = 0; j < N; ++j) 
+        {
+            std::cout << B[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "\nresult matrix:" << std::endl;
+    for (int i = 0; i < N; ++i) 
+    {
+        for (int j = 0; j < N; ++j) 
+        {
+            std::cout << result[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
